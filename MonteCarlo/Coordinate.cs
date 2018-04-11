519041443eb238b3eef46f1e31ad44c8cb212c0f
b-ulak002 <36213365+b-ulak002@users.noreply.github.com>
2018-04-10 20:09:45 -0400

    Add project files.

---
+++ MonteCarlo/Coordinate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MonteCarlo
+{
+    public struct Coordinate
+    {
+        private double x, y;
+        public double X { get => x; set => x = value; }
+        public double Y { get => y; set => y = value; }
+        public Coordinate(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public Coordinate(Random random)
+        {
+            this.x = random.NextDouble();
+            this.y = random.NextDouble();
+        }
+        public double Hypotenuse()
+        {
+            return Math.Sqrt(Math.Pow(this.x, 2) + Math.Pow(this.Y, 2));
+        }
+    }
+}
---
+++ MonteCarlo/TestCoordinate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonteCarlo
+{
+    class TestCoordinate
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter a number: ");
+                int arrayLength = Convert.ToInt32(Console.ReadLine());
+                Coordinate[] arrayOfCordinate = new Coordinate[arrayLength];
+                Random random = new Random();
+                int counter = 0;
+                for (int i = 0; i < arrayLength; i++)
+                {
+                    arrayOfCordinate[i] = new Coordinate(random);
+
+                    if (arrayOfCordinate[i].Hypotenuse() <= 1)
+                    {
+                        counter++;
+                    }
+                }
+                double avg = (counter / (double)arrayOfCordinate.Length) * 4;
+                Console.WriteLine($"Total # of points overlapping the circle = {counter}");
+                Console.WriteLine($"generated pi = {avg}, real pi = {Math.PI}, difference = {Math.Abs(Math.PI - avg)}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
